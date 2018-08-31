ee7108ab815eebb289d2fd0012abc25ae4311bf4
unknown <yashasvij28@gmail.com>
2018-08-31 09:31:48 +0530

    completed assignment 11

--- A11/A11QUES1.py
+++ A11/A11QUES1.py
@@ -1,7 +1,6 @@
 import re
 mail = input("enter a valid email address")
-m =  re.finditer('\w[_a-zA-Z0-9.]*(@)(gmail.com|yahoo.com)' , 'mail')
-if mail = True:
+if re.finditer('\w[_a-zA-Z0-9.]*(@)(gmail.com|yahoo.com)', mail):
     print("You may proceed.")
 else:
     print("please enter a valid email address")
--- A11/A11QUES2.py
+++ A11/A11QUES2.py
@@ -1,3 +1,6 @@
 import re
-phone = ("Enter the valid phone number.")
-m = re.finditer('^[6-9][0-9]{9}*')
+phone = input("Enter the valid phone number.")
+if re.findall('^[+]{1}[9]{1}[1]{1}[6-9]{1}[0-9]{9}', phone):
+    print("valid number")
+else:
+    print("plz enter a valid phone number")
